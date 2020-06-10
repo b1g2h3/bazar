82af39de51a9574fee20d01115c7d2ac010298db
nicework_cz__git <b1g2h3@seznam.cz>
2020-06-10 07:10:44 +0200

    editttttt

--- includes/classes/DB.class.php
+++ includes/classes/DB.class.php
@@ -48,7 +48,7 @@ class DB
 
             if ($this->_query->execute()) {
                 // echo "Success";
-                $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                // $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
                 $this->_count = $this->_query->rowCount();
             } else {
                 $this->_error = true;
